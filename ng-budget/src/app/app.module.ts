5c1b7f0e4f74c903c847188f10f55d92f4809c50
daMelody <daniel.melody@infosys.com>
2020-02-18 11:51:08 -0500

    table sorting implemented

--- ng-budget/src/app/app.module.ts
+++ ng-budget/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { MatButtonModule } from '@angular/material/button';
+import { MatButtonModule } from "@angular/material/button";
+import { MatTableModule } from "@angular/material/table";
 
-import { AppComponent } from './app.component';
-import { ConnectBarComponent } from './connect-bar/connect-bar.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OverviewComponent } from './overview/overview.component';
-import { TransactionTableComponent } from './transaction-table/transaction-table.component';
+import { AppComponent } from "./app.component";
+import { ConnectBarComponent } from "./connect-bar/connect-bar.component";
+import { OverviewComponent } from "./overview/overview.component";
+import { TransactionTableComponent } from "./transaction-table/transaction-table.component";
 
 @NgModule({
     declarations: [
@@ -16,13 +16,8 @@ import { TransactionTableComponent } from './transaction-table/transaction-table
         OverviewComponent,
         TransactionTableComponent
     ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        MatButtonModule,
-        BrowserAnimationsModule
-    ],
+    imports: [BrowserModule, FormsModule, MatButtonModule, MatTableModule],
     providers: [],
     bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
