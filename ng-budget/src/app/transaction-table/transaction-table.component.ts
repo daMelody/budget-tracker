c9941d179d0ce5ad1838c8d542a17390c5d2263c
daMelody <daniel.melody@infosys.com>
2020-02-18 10:31:25 -0500

    transaction table layout maybe finished

--- ng-budget/src/app/transaction-table/transaction-table.component.ts
+++ ng-budget/src/app/transaction-table/transaction-table.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-transaction-table',
-  templateUrl: './transaction-table.component.html',
-  styleUrls: ['./transaction-table.component.css']
+    selector: "app-transaction-table",
+    templateUrl: "./transaction-table.component.html",
+    styleUrls: ["./transaction-table.component.css"]
 })
 export class TransactionTableComponent implements OnInit {
+    displayedColumns: String[] = [
+        "date",
+        "amount",
+        "account",
+        "type",
+        "description"
+    ];
+    transactions: any[];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
