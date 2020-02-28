41e101a9cb6647b931d06a9463874d2b480eaaef
Daniel Melody <dmwalter12@gmail.com>
2020-02-28 16:10:47 -0500

    account table renders new rows!

--- ng-budget/src/app/account-manager/account-manager.component.ts
+++ ng-budget/src/app/account-manager/account-manager.component.ts
@@ -1,8 +1,19 @@
-import { Component, OnInit } from "@angular/core";
-import { MatTableDataSource } from '@angular/material/table';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatTable } from '@angular/material/table';
 import { SelectionModel } from "@angular/cdk/collections";
 import { Account } from "../type-classes/account/account";
 
+const SOURCE = new Array<Account>();
+let newAct: Account = new Account();
+newAct.number = 0;
+newAct.name = "Bank of America";
+newAct.value = -1000;
+SOURCE.push(newAct);
+let sndAct: Account = new Account();
+sndAct.number = 1;
+sndAct.name = "Discover";
+sndAct.value = -250;
+SOURCE.push(sndAct);
 
 @Component({
     selector: "app-account-manager",
@@ -11,21 +22,20 @@ import { Account } from "../type-classes/account/account";
 })
 export class AccountManagerComponent implements OnInit {
     displayedColumns: String[] = ["select", "number", "name", "value"];
-    accounts: MatTableDataSource<Account> = new MatTableDataSource<Account>();
+    accounts: Array<Account> = new Array<Account>();
     selection: SelectionModel<Account> = new SelectionModel<Account>(true, []);
+    @ViewChild(MatTable,{static: true}) table: MatTable<any>;
 
     newNumber: Number;
     newName: String;
     newValue: Number;
 
     constructor() { }
-    ngOnInit() {
-        this.newNumber = this.accounts.data.length;
-    }
+    ngOnInit() { }
 
     isAllSelected(): Boolean {
         const numSelected = this.selection.selected.length;
-        const numRows = this.accounts.data.length;
+        const numRows = this.accounts.length;
         return numSelected === numRows;
     }
 
@@ -33,16 +43,16 @@ export class AccountManagerComponent implements OnInit {
     masterToggle() {
         this.isAllSelected() ?
             this.selection.clear() :
-            this.accounts.data.forEach(row => this.selection.select(row));
+            this.accounts.forEach(row => this.selection.select(row));
     }
 
     addAccount(): void {
         let newAccount: Account = new Account();
-        newAccount.number = this.newNumber;
+        newAccount.number = this.accounts.length;
         newAccount.name = this.newName;
         newAccount.value = this.newValue;
-        this.accounts.data.push(newAccount);
-        this.accounts.connect();
+        this.accounts.push(newAccount);
         console.log(this.accounts);
+        this.table.renderRows();
     }
 }
--- ng-budget/src/app/app.module.ts
+++ ng-budget/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
@@ -25,6 +26,7 @@ import { AccountManagerComponent } from './account-manager/account-manager.compo
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         FormsModule,
         MatFormFieldModule,
         MatButtonModule,
